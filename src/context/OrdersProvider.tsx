574791df47203b0acb70fa62fc18816da09d85ad
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-02-21 14:16:14 +0200

    refactor: update Order import path in OrdersProvider and remove commented initial state

--- src/context/OrdersProvider.tsx
+++ src/context/OrdersProvider.tsx
@@ -5,7 +5,7 @@ import {
   ReactNode,
   useMemo,
 } from "react";
-import { Order } from "../types/types";
+import { Order } from "../../../dashboard/src/types/types";
 
 // Define the context type
 type TOrdersContext = {
@@ -14,51 +14,7 @@ type TOrdersContext = {
   dispatch: React.Dispatch<OrderAction>;
 };
 
-// Define the initial state structure
-// export const initialState: {
-//   currentOrder: Order | null;
-//   orders: Order[];
-// } = {
-//   currentOrder: null,
-//   orders: [
-//     {
-//       id: "1",
-//       client: {
-//         name: "John Doe",
-//         phone: "+1 234-567-8900",
-//         address: "123 Main St, City, Country",
-//         email: "john@example.com",
-//         notes: "Fragile items",
-//       },
-//       packageId: "PKG001",
-//       status: "pending",
-//     },
-//     {
-//       id: "2",
-//       client: {
-//         name: "Jane Smith",
-//         phone: "+1 234-567-8901",
-//         address: "456 Oak St, City, Country",
-//         email: "jane@example.com",
-//         notes: "Express delivery",
-//       },
-//       packageId: "PKG002",
-//       status: "shipped",
-//     },
-//     {
-//       id: "3",
-//       client: {
-//         name: "Mahmoud",
-//         phone: "+201028800187",
-//         address: "Egypt",
-//         email: "mail@example.com",
-//         notes: "I like it",
-//       },
-//       packageId: "PKG002",
-//       status: "compeleted",
-//     },
-//   ],
-// };
+
 
 export const initialState: {
   currentOrder: Order | null;
